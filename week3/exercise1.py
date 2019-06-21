be967eee316f60de2ae2cec5b3f0c4798e09f0b6
Potato1806 <51425938+Potato1806@users.noreply.github.com>
2019-06-21 12:12:56 +1000

    Rip

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,12 @@ def loop_ranger(start, stop=None, step=1):
     The look up the docs for range(), you can answer this with just the range 
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+    ranger = []
+    while stop > start:
+        ranger.append(int(start))
+        start = start + step
+    return ranger
+
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +25,11 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    sth = []
+    while stop > start:
+        sth.append(int(start))
+        start = start + step
+    return sth
 
 
 def two_step_ranger(start, stop):
@@ -29,7 +38,13 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
+    two_step_ranger = []
+    step = 2
+    while stop > start:
+        two_step_ranger.append(int(start))
+        start = start + step
+    return two_step_ranger
+   
 
 
 def stubborn_asker(low, high):
@@ -40,7 +55,7 @@ def stubborn_asker(low, high):
 
     Look up the docs for input
     """
-    return None
+    return 
 
 
 def not_number_rejector(message):
